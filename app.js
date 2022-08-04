1d96291ca839cddc70312b781fc0a90ffe1444fb
nikhilKumar131 <109893645+nikhilKumar131@users.noreply.github.com>
2022-08-04 12:14:48 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var path = require('path');
+var app = express();
+
+//code here
+
+app.get("/", function(req,res){
+    res.sendFile(path.join(__dirname,'/index.html'))
+
+});
+
+
+
+app.listen(3000);
+console.log('listening to port 3000');
